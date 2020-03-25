d525e9058d7ab6eb0212bb56137517caa872d446
Amol Thacker <amolthacker@gmail.com>
2020-03-25 12:33:27 -0400

    adding customizations for experiments

---
+++ 5_experiments.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import cdsw
+
+model_endpoint = 'https://modelservice.ml-5d0a30a0-75c.cdp-cldr.ylcu-atmi.cloudera.site/model'
+access_key = "myvkdz4m0suxv482hon1ssyay15ft09y"
+
+monthly_charges = 70.00
+total_charges = 1400.00
+tenure = 30
+
+if len(sys.argv) > 1:
+  monthly_charges = float(sys.argv[1])
+  total_charges = float(sys.argv[2])
+  tenure = float(sys.argv[3])
+
+model_input = {"StreamingTV":"No","MonthlyCharges":monthly_charges,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":total_charges,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":tenure,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"}
+data = {"accessKey":access_key, "request":model_input}
+headers = {'Content-Type': 'application/json'}
+
+r = requests.post(model_endpoint, data=json.dumps(data), headers=headers)
+churn_probability = (json.loads(r.json()["response"])["probability"])
+
+
+cdsw.track_metric("monthly_charges",round(monthly_charges,2))
+cdsw.track_metric("tenure",round(tenure,2))
+cdsw.track_metric("churn_probability",round(churn_probability,5))
